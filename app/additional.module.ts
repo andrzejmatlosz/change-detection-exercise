d83ad2abee16fb17e473c287b221d5d6d28e171e
Matłosz <ajms@gft.com>
2016-12-09 10:47:03 +0100

    entry point to exercise

--- app/additional.module.ts
+++ app/additional.module.ts
@@ -1,12 +1,10 @@
 
 import { NgModule }      from '@angular/core';
-import { CommonModule }      from '@angular/common';
 
 import { Component6Component } from './secondLevelComponents/component6.component';
 import { Component10Component } from './thirdLevelComponents/component10.component';
 
 @NgModule({
-    imports: [CommonModule],
     declarations: [ 
         Component6Component,
         Component10Component,
